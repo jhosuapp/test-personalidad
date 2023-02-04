5ae4fa4fdb2cb6150bc2517ebed4ea3535cdb507
jhosuapp <jhosuapvll@gmail.com>
2023-02-04 13:36:55 -0800

    first commit

---
+++ sources/js/front.js
@@ -0,0 +1,6 @@
+import { mifuncion } from "./modules/test";
+import "../sass/main.scss";
+
+window.addEventListener('load', ()=>{
+    mifuncion();
+});
---
+++ sources/js/modules/test.js
@@ -0,0 +1,3 @@
+export const mifuncion = ()=>{
+    console.log('Javascript funcionando');
+}
